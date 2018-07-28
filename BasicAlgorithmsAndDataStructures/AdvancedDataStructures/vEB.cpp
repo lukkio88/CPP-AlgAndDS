8a41de95c6d99a54f9f54d7098525fa5ea732077
lukkio88 <luca.gagliano.1988@gmail.com>
2018-07-28 22:17:24 +0100

    Implemented successor/predecessor/insert/delete in vEB tree, the implementation is recursive, not tested

--- BasicAlgorithmsAndDataStructures/AdvancedDataStructures/vEB.cpp
+++ BasicAlgorithmsAndDataStructures/AdvancedDataStructures/vEB.cpp
@@ -1,5 +1,30 @@
 #include <vEB.h>
 
+int upperSqrt(const int & u) {
+	return ceil(log(sqrt(u)));
+}
+
+int lowerSqrt(const int & u) {
+	return floor(log(sqrt(u)));
+}
+
+int high(const int & x, const int & u) {
+	return x / lowerSqrt(u);
+}
+
+int low(const int & x, const int & u) {
+	return x % lowerSqrt(u);
+}
+int index(const int & x, const int & y, const int & u) {
+	return x * lowerSqrt(u) + y;
+}
+
+void swap(int & a, int & b) {
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+
 bool isMember(vEBNode * V, const int & x) {
 	if (V->u == 2) {
 		if (x == V->min || x == V->max)
@@ -11,28 +36,33 @@ bool isMember(vEBNode * V, const int & x) {
 }
 
 int successor(vEBNode * V, const int & x) {
-	if (V->u == 2) {
-		if (x == 0 && V->max == 1)
+	int & min = V->min;
+	int & max = V->max;
+	int & u = V->u;
+	if (u == 2) {
+		if (x == 0 && V->max != -1)
 			return 1;
 		else
 			return -1;
 	}
-	else if (V->min != -1 && x < V->min) {
-		return V->min;
-	}
 	else {
-		int max_low = V->cluster[high(x, V->u)]->max;
-		if (max_low != -1 && x < max_low) {
-			int offset = successor(V->cluster[high(x, V->u)], low(x, V->u));
-			return index(high(x, V->u), offset,V->u);
-		}
+		if (min != -1 && x < min)
+			return min;
 		else {
-			int succ_cluster = successor(V->summary, high(x, V->u));
-			if (succ_cluster == -1)
-				return -1;
+			int max_low = V->cluster[high(x,u)]->max;
+			if (max != -1 && x < max_low) {
+				int offset = successor(V->cluster[high(x, u)], low(x,u));
+				return index(high(x,u), offset,u);
+			}
 			else {
-				int offset = V->cluster[succ_cluster]->min;
-				return index(succ_cluster, offset, V->u);
+				int cluster_idx = successor(V->summary, high(x,u));
+				if (cluster_idx != -1) {
+					int offset = V->cluster[cluster_idx]->min;
+					return index(cluster_idx, offset, u);
+				}
+				else {
+					return -1;
+				}
 			}
 		}
 	}
@@ -40,32 +70,108 @@ int successor(vEBNode * V, const int & x) {
 
 
 int predecessor(vEBNode * V, const int & x) {
-	if (V->u == 2) {
-		if (x == 1 && V->min == 0)
+	int & min = V->min;
+	int & max = V->max;
+	int & u = V->u;
+	if (u == 2) {
+		if (x == 1 && V->min != -1)
 			return 0;
-		else
+		else {
 			return -1;
-	}
-	else if (V->max != -1 && x > V->max) {
-		return V->max;
+		}
 	}
 	else {
-		int min_low = V->cluster[high(x,V->u)]->min;
-		if (min_low != -1 && x > min_low) {
-			int offset = predecessor(V->cluster[high(x, V->u)], low(x, V->u));
-			return index(high(x, V->u), offset,V->u);
+		if (max != -1 && x > max) {
+			return max;
 		}
 		else {
-			int pred_cluster = predecessor(V->summary, high(x, V->u));
-			if (pred_cluster == -1)
-				if (V->min != -1 && x > V->min)
-					return V->min;
-				else
-					return -1;
-			else {
-				int offset = V->cluster[pred_cluster]->max;
-				return index(pred_cluster, offset, V->u);
+			int min_high = V->cluster[high(x, u)]->min;
+			if (min_high != -1) {
+				int offset = predecessor(V->cluster[high(x, u)], low(x,u));
+				return index(high(x, u), offset,u);
 			}
+			else {
+				int cluster_idx = predecessor(V->summary, high(x, u));
+				if (cluster_idx != -1) {
+					if (min != -1)
+						return min;
+					else return -1;
+				}
+				else {
+					int offset = V->cluster[cluster_idx]->max;
+					return index(cluster_idx, offset, u);
+				}
+			}
+		}
+	}
+}
+
+void insertEmpty(vEBNode * v, const int & x) {
+	v->min = v->max = x;
+}
+
+void insert(vEBNode * V, const int & x) {
+	int & min = V->min;
+	int & max = V->max;
+	int & u = V->u;
+	int _x = x;
+	if (min == -1) {
+		insertEmpty(V, x);
+	}
+	else {
+		if (_x < min) {
+			swap(_x, min);
+		}
+		if (u > 2) {
+			if (V->cluster[high(_x,u)]->min == -1) {
+				insert(V->summary, high(_x, u));
+				insertEmpty(V->cluster[high(_x, u)], low(_x,u));
+			}
+			else {
+				insert(V->cluster[high(_x, u)], low(_x, u));
+			}
+		}
+		if (_x > max) {
+			max = _x;
+		}
+	}
+}
+
+void remove(vEBNode * V, const int & x) {
+	int & min = V->min;
+	int & max = V->max;
+	int & u = V->u;
+	if (min == max) {
+		min = max = -1;
+	}
+	else if (u == 2) {
+		if (x == 0)
+			min = 1;
+		else
+			min = 0;
+		max = min;
+	}
+	else {
+		int _x = x;
+		if (_x == min) {
+			int cluster_idx = V->summary->min;
+			_x = index(cluster_idx, V->cluster[cluster_idx]->min,u);
+		}
+		min = _x;
+		remove(V->cluster[high(_x, u)], low(_x, u));
+		if (V->cluster[high(_x, u)]->min == -1) {
+			remove(V->summary, high(_x, u));
+			if (_x == max) {
+				int cluster_idx = V->summary->max;
+				if (cluster_idx != -1) {
+					max = index(cluster_idx, V->cluster[cluster_idx]->max, u);
+				}
+				else
+					max = min;
+			}
+		}
+		else if (_x == max) {
+			max = index(high(_x, u), V->cluster[high(_x, u)]->max,u);
 		}
 	}
 }
--- BasicAlgorithmsAndDataStructures/AdvancedDataStructures/vEB.h
+++ BasicAlgorithmsAndDataStructures/AdvancedDataStructures/vEB.h
@@ -1,12 +1,14 @@
 #pragma once
 #ifndef __VEB_H
 #define __VEB_H
+#include <cmath>
 
 int upperSqrt(const int & u);
 int lowerSqrt(const int & u);
 int high(const int & x, const int & u);
 int low(const int & x, const int & u);
 int index(const int & x, const int & y, const int & u);
+void swap(int & a, int & b);
 
 struct vEBNode {
 
@@ -31,5 +33,7 @@ struct vEBNode {
 bool isMember(vEBNode * V, const int & x);
 int successor(vEBNode * V, const int & x);
 int predecessor(vEBNode * V, const int & x);
+void insert(vEBNode * V, const int & x);
+void remove(vEBNode * V, const int & x);
 
 #endif
